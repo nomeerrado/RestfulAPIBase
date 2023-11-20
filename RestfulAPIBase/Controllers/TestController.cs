be9244aa04e81fff73cb64d6fb09208addfb35bc
Gean <r3k4hg4m3r@gmail.com>
2023-11-20 13:09:23 -0300

    Adicionar arquivos de projeto.

---
+++ RestfulAPIBase/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestfulAPIBase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Status()
+        {
+            return Ok("API is up!");
+        }
+    }
+}
